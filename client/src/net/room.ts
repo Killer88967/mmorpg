90eb81a6c5485fed31a9570539817b38fc4abc8b
Killer88967 <Erniebug27@gmail.com>
2026-09-13 05:20:59 +0000

    Fixed some issues

--- client/src/net/room.ts
+++ client/src/net/room.ts
@@ -1,13 +1,18 @@
-// @ts-nocheck
 import { Client } from "@colyseus/sdk";
 import type { WorldState } from "@/server/schema/WorldState";
 
-export const ENDPOINT = location.hostname.endsWith(".app.github.dev")
+const codespacesEndpoint = location.hostname.endsWith(".app.github.dev")
   ? `${location.protocol}//${location.hostname.replace("-5173.", "-2567.")}`
-  : "http://localhost:2567";
+  : undefined;
 
-export async function createRoom(name) {
-  const client = new Client(ENDPOINT);
+export const ENPOINT =
+  import.meta.env.VITE_SERVER_URL ??
+  codespacesEndpoint ??
+  "https://localhost:2567";
+
+export async function createRoom(name: string) {
+  const client = new Client(ENPOINT);
   const room = await client.joinOrCreate<WorldState>("world", { name });
+
   return { client, room };
 }
