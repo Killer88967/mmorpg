096b07ea524f6b4f1b7757c101247a4e834c7fcb
Killer88967 <Erniebug27@gmail.com>
2026-09-13 05:22:44 +0000

    Typo

--- client/src/net/room.ts
+++ client/src/net/room.ts
@@ -5,13 +5,13 @@ const codespacesEndpoint = location.hostname.endsWith(".app.github.dev")
   ? `${location.protocol}//${location.hostname.replace("-5173.", "-2567.")}`
   : undefined;
 
-export const ENPOINT =
+export const ENDPOINT =
   import.meta.env.VITE_SERVER_URL ??
   codespacesEndpoint ??
-  "https://localhost:2567";
+  "http://localhost:2567";
 
 export async function createRoom(name: string) {
-  const client = new Client(ENPOINT);
+  const client = new Client(ENDPOINT);
   const room = await client.joinOrCreate<WorldState>("world", { name });
 
   return { client, room };
